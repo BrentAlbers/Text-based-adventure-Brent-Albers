69418e1378711c08aacb52d3037c80b24bfcb8ba
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-09-08 12:42:04 +0200

    changes

--- Text-based adventure/Program.cs
+++ Text-based adventure/Program.cs
@@ -23,17 +23,23 @@ namespace Text_based_adventure
         {
             PrintTextLetterByLetter("Guide: Hello  i will be your guide in this adventure.\nGuide: I don't have name yet so first i want you to think of a name for me.\n", 20);
             Console.Write("Enter name for guide here: ");
+            
+            string guide = Console.ReadLine();
+            Console.Clear();
 
-            string Guide = Console.ReadLine();
+            int guideSpaces = guide.Count(char.IsLetter);
+            string spaces = new string(' ', guideSpaces);
 
-            PrintTextLetterByLetter($"{Guide}: What a great name! from now on i will be {Guide}.", 20);
-            PrintTextLetterByLetter($"{Guide}: Now i just need to know your name before we start with this adventure.\n", 20);
+            PrintTextLetterByLetter($"\n{guide}: What a great name! from now on i will be {guide}.", 20);
+            PrintTextLetterByLetter($"{guide}: Now i just need to know your name before we start with this adventure.\n", 20);
 
             Console.Write("Enter your name here: ");
 
-            string Player = Console.ReadLine();
+            string playerName = Console.ReadLine();
+            Console.Clear();
 
-            PrintTextLetterByLetter($"\n{Guide}: Hello {Player}", 20);
+            PrintTextLetterByLetter($"\n{guide}: Now that we know each other, it's time to embark on your first quest, {playerName}.,\n{spaces}  Your mission is to find the hidden map that leads to the location of the Holy Grail.\n ", 20);
+            PrintTextLetterByLetter($"{guide}: Someone is going to try to steal the holy grail too,\n{spaces}  now you need to steal it before it gets stolen", 20);
 
 
         }
